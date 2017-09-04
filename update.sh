52f17ad829c48ce7fcc96fb6cda33707e10f6249
Robert McQueen <rob@endlessm.com>
2017-09-04 22:28:13 +0100

    switch to git for source, update to 5.4.1.2 tag

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-branch=libreoffice-5.4.1.1
+branch=libreoffice-5.4.1.2
 
 wget -O download.lst "https://gerrit.libreoffice.org/gitweb?p=core.git;a=blob_plain;f=download.lst;hb=${branch}"
 wget -O LibreOfficeFlatpak.conf "https://gerrit.libreoffice.org/gitweb?p=core.git;a=blob_plain;f=distro-configs/LibreOfficeFlatpak.conf;hb=${branch}"
